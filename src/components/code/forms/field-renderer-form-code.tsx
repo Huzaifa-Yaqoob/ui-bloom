9664c3ed0b8e50912e41a19ea561a377f198a28c
Muhammad Huzaifa Yaqoob <muhammadhuzaifayaqoob3@gmail.com>
2025-06-10 22:15:25 +0500

    fix export issue

--- src/components/code/forms/field-renderer-form-code.tsx
+++ src/components/code/forms/field-renderer-form-code.tsx
@@ -50,7 +50,7 @@ export default function FieldRendererForm() {
 }
 `;
 
-export const useode = `<FormField
+const useode = `<FormField
   control={form.control}
   name="username"
   render={({ field }) => (
@@ -59,5 +59,7 @@ export const useode = `<FormField
     </FieldRenderer>
   )}
 />`;
+
+export { useode };
 
 export default fieldRendererCode;
--- src/components/code/forms/password-input-code.tsx
+++ src/components/code/forms/password-input-code.tsx
@@ -50,7 +50,7 @@ export function PasswordInputForm() {
 }
 `;
 
-export const useCode = `<FormField
+const useCode = `<FormField
   control={form.control}
   name="password"
   render={({ field }) => (
@@ -59,5 +59,7 @@ export const useCode = `<FormField
     </FieldRenderer>
   )}
 />`;
+
+export { useCode };
 
 export default passwordInputCode;
