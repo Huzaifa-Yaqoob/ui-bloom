b2ecc01626da16c15d3f2047420e75a0ca24c151
Muhammad Huzaifa Yaqoob <muhammadhuzaifayaqoob3@gmail.com>
2026-04-05 10:45:27 +0500

    added packaegs using byn now

--- .content-collections/generated/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { GetTypeByName } from "@content-collections/core";
-
-import configuration from "../../content-collections.ts";
-
-export type Post = GetTypeByName<typeof configuration, "posts">;
-export declare const allPosts: Array<Post>;
-
-export {};
