aefeeabf6e472cbc17663f0a865e90c1579824d1
Muhammad Huzaifa Yaqoob <muhammadhuzaifayaqoob3@gmail.com>
2026-04-12 18:00:21 +0500

    complete home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import Logo from "@/components/common/Logo";
 import PageSection from "@/components/common/Page";
+import { Typography } from "@/components/common/Typography";
 
 export default function Posts() {
-  return <PageSection className={"grid place-items-center"}>Page</PageSection>;
+  return (
+    <PageSection className={"grid place-items-center"}>
+      <div className={"flex flex-col items-center gap-8"}>
+        <Logo className={"size-100"} />
+        <Typography render={<h1 />}>UI Bloom</Typography>
+      </div>
+    </PageSection>
+  );
 }
